5f8e720e321ea06114eea2b5d2496bc9d4dd16d5
pires lufungula <pires00243@gmail.com>
2024-08-12 13:13:37 -0400

    init

---
+++ src/main/java/com/ndako_pay/ndako_pay/NdakoPayApplication.java
@@ -0,0 +1,13 @@
+package com.ndako_pay.ndako_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NdakoPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NdakoPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndako_pay/ndako_pay/NdakoPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndako_pay.ndako_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdakoPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
