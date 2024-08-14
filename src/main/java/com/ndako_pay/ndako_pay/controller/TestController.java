580fd82946b671e652a71bdc2b9871b7e04a188f
pires lufungula <pires00243@gmail.com>
2024-08-14 11:32:03 -0400

    Clean code

--- src/main/java/com/ndako_pay/ndako_pay/controller/TestController.java
+++ src/main/java/com/ndako_pay/ndako_pay/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.ndako_pay.ndako_pay.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ndekocash")
 public class TestController {
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public String home() {
 		return "test loll";
 	}
