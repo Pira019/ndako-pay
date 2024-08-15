6c360e0add42195677fb7e21d16c0987cf5389bc
pires lufungula <pires00243@gmail.com>
2024-08-15 15:14:47 -0400

    lo

--- src/main/java/com/ndako_pay/ndako_pay/NdakoPayApplication.java
+++ src/main/java/com/ndako_pay/ndako_pay/NdakoPayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NdakoPayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NdakoPayApplication.class, args);
+		SpringApplication.run(NdakoPayApplication.class, args );
 	}
 
 }
